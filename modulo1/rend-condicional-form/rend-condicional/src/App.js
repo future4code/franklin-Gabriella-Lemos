f608d92ec5476f93fa4221cf3e6b9d8c999ba0a4
colaborador <colaborador@N091153.local>
2022-06-07 21:51:32 -0300

    Adicionando componente e renderizacao no formulário

--- modulo1/rend-condicional-form/rend-condicional/src/App.js
+++ modulo1/rend-condicional-form/rend-condicional/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Etapa01 } from "./components/Etapa01";
+import { Etapa02 } from "./components/Etapa02";
+import { Etapa03 } from "./components/Etapa03";
+import { Fimform } from "./components/Fimform";
 
 function App() {
+  const [state, setState] = useState("form01");
+
+  const passaForm = () => {
+    if (state === "form01") {
+      return setState("form02");
+    }
+    if (state === "form02") {
+      return setState("form03");
+    }
+    if (state === "form03") {
+      return setState("form04");
+    }
+  };
+
+  const FormRender = () => {
+    switch (state) {
+      case "form01":
+        return <Etapa01 />;
+        break;
+
+      case "form02":
+        return <Etapa02 />;
+        break;
+
+      case "form03":
+        return <Etapa03 />;
+        break;
+
+      case "form04":
+        return <Fimform />;
+        break;
+
+      default:
+        break;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{FormRender()}</p>
+      <button onClick={passaForm}>Próxima Etapa</button>
     </div>
   );
 }
