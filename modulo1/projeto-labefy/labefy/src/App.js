979edbe773a1f2a2133bd50b1192f17c11ea09fa
colaborador <colaborador@N091153.local>
2022-06-17 19:08:38 -0300

    adicionando playlist

--- modulo1/projeto-labefy/labefy/src/App.js
+++ modulo1/projeto-labefy/labefy/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [inputNamePlaylist, setInputNamePlaylist] = useState("");
+  const [playlists, setPlaylists] = useState([]);
+
+  const handlePlaylist = (event) => {
+    setInputNamePlaylist(event.target.value);
+  };
+
+  const createPlaylist = () => {
+    const body = {
+      name: inputNamePlaylist,
+    };
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        body,
+        {
+          headers: {
+            Authorization: "gabriella-lemos-franklin",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setInputNamePlaylist("");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  const geraPlaylist = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        {
+          headers: {
+            Authorization: "gabriella-lemos-franklin",
+          },
+        }
+      )
+      .then((respponse) => {
+        setPlaylists(respponse.data.result.list);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  };
+
+  const novaPlaylist = playlists.map((lista) => {
+    return <p key={lista.id}>{lista.name}</p>;
+  });
+
+  useEffect(geraPlaylist, [playlists]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Digite o nome da playlist:</h4>
+      <input onChange={handlePlaylist} value={inputNamePlaylist}></input>
+      <button onClick={createPlaylist}>criar playlist</button>
+      {novaPlaylist}
     </div>
   );
 }
