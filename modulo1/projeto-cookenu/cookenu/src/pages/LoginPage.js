3f6bd287126edda9e5175543dd7b7f400ed7b2d8
colaborador <colaborador@N091153.local>
2022-06-26 09:47:01 -0300

    adicionndo imputs de login

--- modulo1/projeto-cookenu/cookenu/src/pages/LoginPage.js
+++ modulo1/projeto-cookenu/cookenu/src/pages/LoginPage.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { goToLists, goToCadastro } from "../routers/coordinator";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleInputSenha = (event) => {
+    setPassword(event.target.value);
+  };
   return (
     <>
       <h2> Login </h2>
+      <input onChange={handleInputEmail} value={email}></input>
+      <input onChange={handleInputSenha} value={password}></input>
+
       <button onClick={() => goToLists(navigate)}>lista de receitas</button>
       <button onClick={() => goToCadastro(navigate)}>cadastre-se</button>
     </>
