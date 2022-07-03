6f959c3ca8ae90384960b95157dac68c0dde2931
colaborador <colaborador@N091153.local>
2022-07-03 15:45:18 -0300

    adicionando pagina de receitas

--- modulo1/projeto-cookenu/cookenu/src/pages/RecipeListPage/RecipeListPage.js
+++ modulo1/projeto-cookenu/cookenu/src/pages/RecipeListPage/RecipeListPage.js
@@ -1,19 +1,46 @@
 import React from "react";
-// import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import useProtectedPage from "../../hooks/useProtectedPage";
+import useRequestData from "../../hooks/useRequestData";
+import { BASE_URL } from "../../constants/urls";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import { AddRecipeButton, RecipeListContainer } from "./styled";
+import AddIcon from "@mui/icons-material/Add";
+import { goToAddRecipesPage } from "../../routers/coordinator";
 
 const RecipeListPage = () => {
+  const navigate = useNavigate();
+
   useProtectedPage();
+
+  const onClickCard = () => {};
+
+  const recipes = useRequestData([], `${BASE_URL}/recipe/feed`);
+
+  const recipeCards = recipes.map((recipe) => {
+    return (
+      <RecipeCard
+        key={recipe.recipeid}
+        title={recipe.title}
+        image={recipe.image}
+        onClick={() => null}
+      />
+    );
+  });
+
+  console.log(recipes);
+
   // const navigate = useNavigate();
   return (
     <>
-      <h2> Lista de receitas</h2>
-      <Button color="primary" variant="contained">
-        neutral
-      </Button>
-      {/* <button onClick={() => goToDetails(navigate)}>Detalhar receita</button>
-      <button onClick={() => goToCreate(navigate)}>Adicionar receita</button> */}
+      <RecipeListContainer>{recipeCards}</RecipeListContainer>
+      <AddRecipeButton
+        color="primary"
+        onClick={() => goToAddRecipesPage(navigate)}
+      >
+        {" "}
+        <AddIcon />
+      </AddRecipeButton>
     </>
   );
 };
