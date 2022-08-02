4683e7c05d69d3104a7b088def0d083663c8a8e3
colaborador <colaborador@N091153.local>
2022-08-02 20:31:31 -0300

    adicionando exercícios de node

---
+++ modulo2/node-package-json/index.js
@@ -0,0 +1,4 @@
+console.log(
+  "acessar os parâmetros passados na linha de comando devemos usar o progress.argv[i]"
+);
+console.log("Olá,", process.argv[2], "! Você tem", process.argv[3], "anos.");
