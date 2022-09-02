fd0f759d00d8d7907ed51f32e82ce7304d97b9b1
colaborador <colaborador@N091153.local>
2022-09-01 21:45:15 -0300

    finalizando exercicios propostos

--- modulo2/heranca-polimorfismo/exercicios-heranca-polimorfismo/src/index.ts
+++ modulo2/heranca-polimorfismo/exercicios-heranca-polimorfismo/src/index.ts
@@ -9,7 +9,12 @@
 // exercício 03
 //Não é possivel acessar o atributo password, pois a classe User(pai) não disponibiliza métodos acessores para o atributo password.
 
+import { Client } from "./Client";
+import { Commerce } from "./Commerce";
 import { Customer } from "./Customer";
+import { Industry } from "./Industry";
+import { Place } from "./Place";
+import { Residence } from "./Residence";
 import { User } from "./User";
 
 // const Gabi = new User("000000", "gsbi@email.com", "Gabriella", "123456");
@@ -22,9 +27,38 @@ const Ana = new Customer(
   "1234566789"
 );
 
-console.log(Ana.getId());
-console.log(Ana.getEmail());
-console.log(Ana.getEmail());
-console.log(Ana.getCreditCard());
-console.log(Ana.purchaseTotal);
-Ana.introduceYourself();
+// console.log(Ana.getId());
+// console.log(Ana.getEmail());
+// console.log(Ana.getEmail());
+// console.log(Ana.getCreditCard());
+// console.log(Ana.purchaseTotal);
+// Ana.introduceYourself();
+
+const client: Client = {
+  name: "Goli",
+  registrationNumber: 1,
+  consumedEnergy: 100,
+
+  calculateBill: () => {
+    return 2;
+  },
+};
+
+console.log(client.name);
+console.log(client.registrationNumber);
+console.log(client.consumedEnergy);
+client.calculateBill();
+
+// const teste = new Place("20340-456"); Erro: Não é possível instanciar classe abstrata.
+
+const Residence01 = new Residence(2, "23456-890");
+
+const Commerce01 = new Commerce(5, "23456-089");
+
+const Industry01 = new Industry(20, "45566-768");
+
+console.log(Residence01.getCep());
+
+console.log(Commerce01.getCep());
+
+console.log(Industry01.getCep());
